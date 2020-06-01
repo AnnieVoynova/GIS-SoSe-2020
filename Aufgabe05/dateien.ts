2b686dd45a6fe60dcd8328e470201b223efca9b9
Annie <antoaneta.voynova@hs-furtwangen.de>
2020-06-01 16:23:58 +0200

    1 interface

--- Aufgabe05/dateien.ts
+++ Aufgabe05/dateien.ts
@@ -1,5 +1,5 @@
 namespace Aufgabe05 {
-interface ArtikelnInking { //deklaration der variable
+interface Artikeln { //deklaration der variable
     name: string;
     beschreibung: string;
     preis: string;
@@ -7,45 +7,30 @@ interface ArtikelnInking { //deklaration der variable
     photo: string;
 }
 // Artikeln erstelen
-let pen: ArtikelnInking = {name: "Faber Castell PITT Artist Pen", photo: "images/markers5.jpg", beschreibung: "Set of 8,black", preis: "15€", category: "inking" };
-let artistpen: ArtikelnInking = {name: "Faber Castell PITT Artist Pen", photo: "images/markers6.jfif", beschreibung: "Shades of Grey, Set of 6 soft brush markers", preis: "12.50€", category: "inking" };
-let inkpen: ArtikelnInking = {name: "Ink Pen", photo: "images/ink8.jpg", beschreibung: "Set of 4 different sizes", preis: "15€", category: "inking" };
-let ink: ArtikelnInking = {name: "Drawing Ink", photo: "images/ink9.jpg", beschreibung: "SDifferent Colours available,78ml", preis: "4€ per Bottle", category: "inking" };
+let pen: Artikeln = {name: "Faber Castell PITT Artist Pen", photo: "images/markers5.jpg", beschreibung: "Set of 8,black", preis: "15€", category: "inking" };
+let artistpen: Artikeln = {name: "Faber Castell PITT Artist Pen", photo: "images/markers6.jfif", beschreibung: "Shades of Grey, Set of 6 soft brush markers", preis: "12.50€", category: "inking" };
+let inkpen: Artikeln = {name: "Ink Pen", photo: "images/ink8.jpg", beschreibung: "Set of 4 different sizes", preis: "15€", category: "inking" };
+let ink: Artikeln = {name: "Drawing Ink", photo: "images/ink9.jpg", beschreibung: "SDifferent Colours available,78ml", preis: "4€ per Bottle", category: "inking" };
 
 
-let artikelnInking: ArtikelnInking[] = [pen, artistpen, inkpen, ink]; //Array
+let artikelnInking: Artikeln[] = [pen, artistpen, inkpen, ink]; //Array
 
-interface ArtikelnDrawing { //deklaration der variable
-    name: string;
-    beschreibung: string;
-    preis: string;
-    category: string;
-    photo: string;
-}
 // Artikeln erstelen
-let watercolors: ArtikelnDrawing = {name: "Koi Watercolour Paints", photo: "images/koiwater14.jpg", beschreibung: "Travel Set-12 Colours", preis: "15€", category: "painting" };
-let paints: ArtikelnDrawing = {name: "Watercolour Paints", photo: "images/paints11.jpg", beschreibung: "Box set of 36 Coours x 59ml", preis: "30€", category: "painting" };
-let pencils: ArtikelnDrawing = {name: "Derwent Watercolour Pencils", photo: "images/waterpencils.jpg", beschreibung: "12 Colours,extremely good quality", preis: "20€", category: "painting" };
-let acryl: ArtikelnDrawing = {name: "Mont Marte Acrylic Paints", photo: "images/paints12.jpg", beschreibung: "Set of 24 Colours x 36ml", preis: "30€", category: "painting" };
-let rowney: ArtikelnDrawing = {name: "Daler Rowney Acrylic Paints", photo: "images/paint13.jpg", beschreibung: "Simply Acrylic Paint Bottle x75ml, Different Colours available", preis: "Preis:5€ per Bottle", category: "painting" };
+let watercolors: Artikeln = {name: "Koi Watercolour Paints", photo: "images/koiwater14.jpg", beschreibung: "Travel Set-12 Colours", preis: "15€", category: "painting" };
+let paints: Artikeln = {name: "Watercolour Paints", photo: "images/paints11.jpg", beschreibung: "Box set of 36 Coours x 59ml", preis: "30€", category: "painting" };
+let pencils: Artikeln = {name: "Derwent Watercolour Pencils", photo: "images/waterpencils.jpg", beschreibung: "12 Colours,extremely good quality", preis: "20€", category: "painting" };
+let acryl: Artikeln = {name: "Mont Marte Acrylic Paints", photo: "images/paints12.jpg", beschreibung: "Set of 24 Colours x 36ml", preis: "30€", category: "painting" };
+let rowney: Artikeln = {name: "Daler Rowney Acrylic Paints", photo: "images/paint13.jpg", beschreibung: "Simply Acrylic Paint Bottle x75ml, Different Colours available", preis: "Preis:5€ per Bottle", category: "painting" };
 
-let artikelnDrawing: ArtikelnDrawing[] = [watercolors, paints, pencils, acryl , rowney]; //Array
+let artikelnDrawing: Artikeln[] = [watercolors, paints, pencils, acryl , rowney]; //Array
 
-
-interface ArtikelnSketchbooks { //deklaration der variable
-    name: string;
-    beschreibung: string;
-    preis: string;
-    category: string;
-    photo: string;
-}
 // Artikeln erstelen
-let sketch: ArtikelnSketchbooks = {name: "Sketchbook Hardcover,Spiral", photo: "images/sketchbook1.jpg", beschreibung: "8,5x11; 80 Pages", preis: "10€", category: "sketchbooks" };
-let sketchrowney: ArtikelnSketchbooks = {name: "Sketchbook D.Rowney", photo: "images/Sketchbook2.jpg", beschreibung: "14x11; 80 pages", preis: "15€", category: "sketchbooks" };
-let spocket: ArtikelnSketchbooks = {name: "Pocket Sketchbook R.Downey", photo: "images/Sketchbook3.jpg", beschreibung: "3,5x5,5; 144 Pages", preis: "12€", category: "sketchbooks" };
-let mixmedia: ArtikelnSketchbooks = {name: "Sketchbook Mix Media", photo: "images/mixmedia.jpg", beschreibung: "7x10in; 60 Seiten", preis: "20€", category: "sketchbooks" };
+let sketch: Artikeln = {name: "Sketchbook Hardcover,Spiral", photo: "images/sketchbook1.jpg", beschreibung: "8,5x11; 80 Pages", preis: "10€", category: "sketchbooks" };
+let sketchrowney: Artikeln = {name: "Sketchbook D.Rowney", photo: "images/Sketchbook2.jpg", beschreibung: "14x11; 80 pages", preis: "15€", category: "sketchbooks" };
+let spocket: Artikeln = {name: "Pocket Sketchbook R.Downey", photo: "images/Sketchbook3.jpg", beschreibung: "3,5x5,5; 144 Pages", preis: "12€", category: "sketchbooks" };
+let mixmedia: Artikeln = {name: "Sketchbook Mix Media", photo: "images/mixmedia.jpg", beschreibung: "7x10in; 60 Seiten", preis: "20€", category: "sketchbooks" };
 
-let artikelnSketchbooks: ArtikelnSketchbooks[] = [sketch, sketchrowney, spocket, mixmedia]; //Array
+let artikelnSketchbooks: Artikeln[] = [sketch, sketchrowney, spocket, mixmedia]; //Array
 
 //Kategorie Inking
 for (let index = 0; index < artikelnInking.length; index++) {
