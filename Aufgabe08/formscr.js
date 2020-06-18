df0173f75a70e9f896d7439867a8246af83cd6a2
Annie <antoaneta.voynova@hs-furtwangen.de>
2020-06-18 18:36:15 +0200

    idk

--- Aufgabe08/formscr.js
+++ Aufgabe08/formscr.js
@@ -4,21 +4,35 @@ var Aufgabe08;
     let button; //= document.getElementById("button");
     button = document.getElementById("button");
     button.addEventListener("click", handleSend);
-    //functions
+    let formData = new FormData(document.forms[0]);
     async function handleSend() {
-        getData(await sendData());
-    }
-    async function sendData() {
-        let formData = new FormData(document.forms[0]);
+        //getData(await sendData());
         let url = "https://testgissomse2020.herokuapp.com/";
+        // tslint:disable-next-line: no-any //quickfix "any" error
         let query = new URLSearchParams(formData);
         url = url + "?" + query.toString();
-        return url;
+        let response = await fetch(url);
+        let ausgabe = await response.text();
+        //let ausgabe = response;
+        console.log(ausgabe);
     }
-    async function getData(_url) {
+    /*for (let entry of formData) {
+    console.log(entry);
+    console.log("name: " + entry[0]);
+    console.log("value: " + entry[1]);
+    }*/
+    /*async function sendData(): Promise<string> { //Daten asynchron senden
+          let formData: FormData = new FormData(document.forms[0]);
+          let url: string = "https://testgissomse2020.herokuapp.com/";
+          let query: URLSearchParams = new URLSearchParams(<any>formData);
+          url = url + "?" + query.toString();
+          return url;
+    }
+
+    async function getData(_url: RequestInfo): Promise<void> { //die response machen
         let response = await fetch(_url);
         let ausgabe = await response.text();
         console.log(ausgabe);
-    }
+    } */
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=formscr.js.map
--- Aufgabe08/formscr.ts
+++ Aufgabe08/formscr.ts
@@ -1,14 +1,32 @@
 namespace Aufgabe08 {
+
     let button: HTMLButtonElement; //= document.getElementById("button");
     button = <HTMLButtonElement> document.getElementById("button");
     button.addEventListener("click", handleSend);
 
-    //functions
+    let formData: FormData = new FormData(document.forms[0]);
+
     async function handleSend(): Promise <void> { //function,die aufgeruft wird wenn man auf den button klickt
-        getData(await sendData());
+        //getData(await sendData());
+        let url: string = "https://testgissomse2020.herokuapp.com/";
+        // tslint:disable-next-line: no-any //quickfix "any" error
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        let response: Response = await fetch(url);
+        let ausgabe: string = await response.text();
+        //let ausgabe = response;
+        console.log(ausgabe); 
     }
     
-    async function sendData(): Promise<string> { //Daten asynchron senden
+      
+    /*for (let entry of formData) {
+    console.log(entry);
+    console.log("name: " + entry[0]);
+    console.log("value: " + entry[1]);
+    }*/
+    
+    
+    /*async function sendData(): Promise<string> { //Daten asynchron senden
           let formData: FormData = new FormData(document.forms[0]);
           let url: string = "https://testgissomse2020.herokuapp.com/";
           let query: URLSearchParams = new URLSearchParams(<any>formData);
@@ -20,6 +38,6 @@ namespace Aufgabe08 {
         let response = await fetch(_url);
         let ausgabe = await response.text();
         console.log(ausgabe);
-    }
+    } */
     
 }
