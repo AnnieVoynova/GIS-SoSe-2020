cf3faea1c70eac5054b78c58c3ac185c18d31ad0
Annie <antoaneta.voynova@hs-furtwangen.de>
2020-06-18 19:06:47 +0200

    final

--- Aufgabe08/formscr.ts
+++ Aufgabe08/formscr.ts
@@ -23,21 +23,5 @@ namespace Aufgabe08 {
     console.log(entry);
     console.log("name: " + entry[0]);
     console.log("value: " + entry[1]);
-    }*/
-    
-    
-    /*async function sendData(): Promise<string> { //Daten asynchron senden
-          let formData: FormData = new FormData(document.forms[0]);
-          let url: string = "https://testgissomse2020.herokuapp.com/";
-          let query: URLSearchParams = new URLSearchParams(<any>formData);
-          url = url + "?" + query.toString();
-          return url;
-    }
-
-    async function getData(_url: RequestInfo): Promise<void> { //die response machen
-        let response = await fetch(_url);
-        let ausgabe = await response.text();
-        console.log(ausgabe);
-    } */
-    
+    }*/ 
 }
